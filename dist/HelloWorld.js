7906c39e48a9e01cb5298524fa574351ca14ac33
Sven ten Haaf <sven.ten.haaf@persgroep.nl>
2020-05-13 17:06:19 +0200

    first commit and publish

---
+++ dist/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloWorld = ({
+  children
+}) => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", null, "HELLO WORLD"), children);
+};
+
+export default HelloWorld;
---
+++ src/DraggableBackground/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = ({ children }) => {
+  return (
+    <div>
+      <div>HELLO WORLD</div>
+      {children}
+    </div>
+  )
+}
+
+export default HelloWorld
